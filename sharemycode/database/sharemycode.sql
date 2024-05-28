6cbaaf6b15783c5de78e8b5ad0368bdfa3c716d3
Charlie <charlie.pierre@orange.fr>
2024-05-28 12:24:20 +0200

    changed queries for table creation

--- sharemycode/database/sharemycode.sql
+++ sharemycode/database/sharemycode.sql
@@ -1,26 +1,24 @@
 CREATE TABLE users(
-   id_users COUNTER,
+   id_users INTEGER PRIMARY KEY AUTOINCREMENT,
    name VARCHAR(50),
    password TEXT,
    created_at DATE NOT NULL,
    updated_at DATE NOT NULL,
-   PRIMARY KEY(id_users)
 );
 
 CREATE TABLE contents(
-   id_contents COUNTER,
+   id_contents INTEGER PRIMARY KEY AUTOINCREMENT,
    content TEXT NOT NULL,
    langage TEXT NOT NULL,
    expiration DATE,
    created_at DATE NOT NULL,
    updated_at DATE NOT NULL,
    id_users INT,
-   PRIMARY KEY(id_contents),
    FOREIGN KEY(id_users) REFERENCES users(id_users)
 );
 
 CREATE TABLE logs(
-   id_logs COUNTER,
+   id_logs INTEGER PRIMARY KEY AUTOINCREMENT,
    created_at DATE,
    id_contents INT NOT NULL,
    id_users INT NOT NULL,
